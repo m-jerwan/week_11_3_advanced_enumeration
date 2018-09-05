ac6788410884c97b31c02308d14810744d4553d1
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-05 17:13:24 +0100

    first two done

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,15 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
-PangramFinder.prototype.isPangram = function () {
-
+PangramFinder.prototype.isPangram = function() {
+  const phraseTolowCase = this.phrase.toLowerCase();
+  const sentenceAsArray = phraseTolowCase.split('');
+  const result  = this.alphabet.every((letter) =>{
+  return sentenceAsArray.includes(letter);
+  })
+  return result;
 }
 
 module.exports = PangramFinder;
--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,14 @@
 const UpperCaser = function (words) {
+    this.words = words;
+
 }
 
 UpperCaser.prototype.toUpperCase = function () {
 
+    const upperCase = this.words.map((word) => {
+        return word.toUpperCase();
+    })
+    return upperCase;
 }
 
 module.exports = UpperCaser;
