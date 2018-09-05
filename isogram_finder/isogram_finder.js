253bcee5182d00a83f1eac61327b69fabe196770
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-05 21:45:34 +0100

    third adv extension- work in progress

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,36 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
+
+// this.journeys.forEach((journey) => {
+//     if (!uniqueModesOfTransport.includes(journey.transport)) {
+//         uniqueModesOfTransport.push(journey.transport);
+//     }
+// })
+
+
+
 
 IsogramFinder.prototype.isIsogram = function () {
+    debugger
+    const wordAsLowCase =  this.word.toLowerCase();
+    const wordAsArrayLowCase = wordAsLowCase.split('');
 
+    let uniqueWordAsArrayLowCase = [];
+
+    wordAsArrayLowCase.forEach((letter) =>{
+        if (!uniqueWordAsArrayLowCase.includes(letter)){
+            uniqueWordAsArrayLowCase.push(letter);
+        }
+    })
+
+    if (uniqueWordAsArrayLowCase ===  wordAsArrayLowCase){
+        return true;
+    }
+    return false;
 }
+
+
+
 
 module.exports = IsogramFinder;
