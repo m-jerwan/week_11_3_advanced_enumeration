3d69b0b93a6e92cd1426245026696126400c4b4a
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-06 09:28:51 +0100

    attempt at Anagram. not working.

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,17 @@
 const AnagramFinder = function (word) {
+    this.word = word;
 
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    const wordAsArray = word.split("");
+    const resultOfFilter = otherWords.filter((otherWord)=> {
+        const resultOfEvery = wordAsArray.every((letter) =>{
+            return otherWord.include(letter);
+        })
+        return resultOfEvery;
+    })
+    return resultOfFilter;
 }
 
 module.exports = AnagramFinder;
